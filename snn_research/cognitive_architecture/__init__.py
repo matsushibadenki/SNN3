2e37f6ac7cdc69b2b17c880b897a9394c2f09f16
matsushibadenki <littlebuddha@mac.com>
2025-10-04 19:43:26 +0900

    Update __init__.py

--- snn_research/cognitive_architecture/__init__.py
+++ snn_research/cognitive_architecture/__init__.py
@@ -1,16 +1,16 @@
 # matsushibadenki/snn3/snn_research/cognitive_architecture/__init__.py
 
 from .astrocyte_network import AstrocyteNetwork
-# ◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️↓修正開始◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️
 from .emergent_system import EmergentCognitiveSystem
-# ◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️↑修正終わり◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️
 from .global_workspace import GlobalWorkspace
 from .hierarchical_planner import HierarchicalPlanner
 from .intrinsic_motivation import IntrinsicMotivationSystem
 from .meta_cognitive_snn import MetaCognitiveSNN
 from .physics_evaluator import PhysicsEvaluator
 from .planner_snn import PlannerSNN
-from .rag_snn import RAGSystemSNN
+# ◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️↓修正開始◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️
+from .rag_snn import RAGSystem
+# ◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️↑修正終わり◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️◾️
 
 __all__ = [
     "AstrocyteNetwork",
@@ -21,5 +21,5 @@ __all__ = [
     "MetaCognitiveSNN",
     "PhysicsEvaluator",
     "PlannerSNN",
-    "RAGSystemSNN"
+    "RAGSystem"
 ]
