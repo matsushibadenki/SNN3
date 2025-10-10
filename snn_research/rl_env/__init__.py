1d1601e1cf3769e6d7a85ed4a82a55ca0e62d1dc
matsushibadenki <littlebuddha@mac.com>
2025-10-10 10:04:58 +0900

    Update __init__.py

--- snn_research/rl_env/__init__.py
+++ snn_research/rl_env/__init__.py
@@ -1,2 +1,8 @@
 # matsushibadenki/snn3/snn_research/rl_env/__init__.py
 # Reinforcement Learning Environment Package
+# 改善点: 新しく作成したGridWorldEnvをインポートする。
+
+from .simple_env import SimpleEnvironment
+from .grid_world import GridWorldEnv
+
+__all__ = ["SimpleEnvironment", "GridWorldEnv"]
