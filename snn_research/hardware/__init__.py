cb64198650f8ea9076d83e4011745347b40b4fc2
matsushibadenki <littlebuddha@mac.com>
2025-10-11 21:19:23 +0900

    Update __init__.py

--- snn_research/hardware/__init__.py
+++ snn_research/hardware/__init__.py
@@ -1,6 +1,11 @@
 # snn_research/hardware/__init__.py
-# (新規作成)
+# (更新)
 
 from .profiles import get_hardware_profile, loihi_profile
+from .compiler import NeuromorphicCompiler
 
-__all__ = ["get_hardware_profile", "loihi_profile"]
+__all__ = [
+    "get_hardware_profile", 
+    "loihi_profile",
+    "NeuromorphicCompiler"
+]
